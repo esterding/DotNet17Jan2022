c48318eaa6721c1f4cbbdbcad2983e4b11dbe083
esterding <esterding86@yahoo.com.sg>
2022-02-10 01:38:22 +0800

    submitWebApp

---
+++ Day16/task090222/task090222/Controllers/ProductController.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using task090222.Models;
+using System.Linq;
+
+namespace task090222.Controllers
+{
+    public class ProductController : Controller
+    {
+        static public List<Product> Products = new List<Product>(){
+
+            new Product { Id = 1, Name = "Spicy Chicken Noodles", Price = 4.50, Quantity = 150, Remarks = "Spicy", SupplierId = 005 },
+            new Product { Id = 2, Name = "Sweet and Sour Chicken Noodles", Price = 4.30, Quantity = 120, Remarks = "Non Spicy", SupplierId = 005 },
+            new Product { Id = 3, Name = "Clear Soup Glass Noodles", Price = 4.00, Quantity = 120, Remarks = "Non Spicy", SupplierId = 006 }
+
+        };
+        public IActionResult Index()
+        {
+            return View(Products);
+        }
+
+        [HttpGet]
+        public IActionResult Create()
+        {
+            return View(new Product());
+        }
+
+        [HttpPost]
+        public IActionResult Create(Product product)
+        {
+            Products.Add(product);
+            return RedirectToAction("Index");
+        }
+
+        [HttpGet]
+        public IActionResult Edit(int productId)
+        {
+            Product product = new Product();
+            product = Products.FirstOrDefault(p => p.Id == productId);
+            return View(product);
+        }
+    }
+}
---
+++ Day16/task090222/task090222/Models/Product.cs
@@ -0,0 +1,12 @@
+﻿namespace task090222.Models
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public double Price { get; set; }
+        public int SupplierId { get; set; }
+        public int Quantity { get; set; }
+        public string Remarks { get; set; }
+    }
+}
